6aee42e72a01fbbdf1fba44b80eb18ef844c917e
mikinator <michaelmaduagwu630@gmail.com>
2026-03-01 13:43:03 +0100

    feat: add button to allow grid size selection

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+const containerSize = 320
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+button.textContent = "set your grid size";
+let gridNo = 16;
+let gridSize = (containerSize/gridNo) - 1;
 
-for (let cellIndex = 0; cellIndex < 256; cellIndex++) {
-    let gridCell = document.createElement("div");
-    gridCell.classList.add("gridCell");
-    container.appendChild(gridCell);
+button.addEventListener("click", () => {
+    gridNo = parseInt(prompt("Set grid size "));
+    gridSize = (containerSize/gridNo) - 1;
+    deleteGrid();
+    createGrid();
+})
+
+function createGrid() {
+    for (let cellIndex = 0; cellIndex < gridNo ** 2; cellIndex++) {
+        let gridCell = document.createElement("div");
+        gridCell.style.width = `${gridSize}px`;
+        gridCell.style.height = `${gridSize}px`;
+        gridCell.classList.add("gridCell");
+        gridCell.addEventListener("mouseenter", () => {
+            gridCell.style.backgroundColor = "blue";
+        })
+        gridCell.addEventListener("mouseleave", () => {
+            gridCell.style.backgroundColor = "red";
+        })
+        container.appendChild(gridCell);
+    }
 }
+
+function deleteGrid() {
+    currentGrid = document.querySelectorAll(".gridCell");
+    currentGrid.forEach(element => {
+        element.remove()
+    });
+}
+
+createGrid()
